81362d9068edcc486e6e6acdecae6105c7e1fc46
kammila-tilak <kammilatilak@gmail.com>
2025-06-17 23:43:37 +0530

    comit

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { createContext,use,useEffect, useState } from "react";
+import { createContext,useEffect, useState } from "react";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
   const [cartItem,setCartItem] = useState({});
-  const url ="http://localhost:4000"
+  const url ="https://food-delivery-3a7e.onrender.com"
   const [token,setToken] = useState("")
   const [food_list,setFoodList] = useState([])
   const [loadingFood, setLoadingFood] = useState(true); // Add loading state
